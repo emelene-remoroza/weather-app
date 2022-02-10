5b1e235cbe86a91c7d0c241815351c9ac79b3609
krissy-legeyt <krissylegeyt@gmail.com>
2022-02-11 10:51:12 +1300

    added city route

--- server/routes/weather.js
+++ server/routes/weather.js
@@ -4,8 +4,20 @@ const request = require('superagent')
 
 const router = express.Router()
 
+// setup for city parameters
+router.get('/:city', (req, res) => {
+  const city = req.params.city
+  return request.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${city}&aqi=no`)
+    .then(response => {
+      return res.json(response.body)
+    })
+    .catch((err) => {
+      res.status(500).send(err.message)
+    })
+})
+
+// hard coded to Auckland
 router.get('/', (req, res) => {
-  console.log(process.env.WEATHER_API_KEY)
   return request.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=Auckland&aqi=no`)
     .then(response => {
       return res.json(response.body)
